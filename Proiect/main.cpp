3e30077a6942e4b6096f351aa9f80c73550e3b95
alexserban26 <alexserban80@gmail.com>
2020-10-13 12:49:09 +0300

    Update

--- Proiect/main.cpp
+++ Proiect/main.cpp
@@ -20,48 +20,48 @@ public:
 };
 
 Elev::Elev(){
-    std::cout<< "Elevul:";
+    std::cout<< "Elevul: ";
     nume = "-";
     prenume = "-";
     numar_clasa=-1;
     litera_clasa= "-";
-    std::cout<<nume<<" "<<prenume<<" Clasa "<<numar_clasa<<litera_clasa;
+    std::cout<<nume<<" "<<prenume<<" Clasa "<<numar_clasa<<" "<<litera_clasa<<"\n\n";
 }
 
 Elev::Elev(std::string nume){
-    std::cout<< "Elevul:";
+    std::cout<< "Elevul: ";
     this->nume = nume;
     prenume = "-";
     numar_clasa=-1;
     litera_clasa= "-";
-    std::cout<<this->nume<<" "<<prenume<<" Clasa "<<numar_clasa<<litera_clasa;
+    std::cout<<this->nume<<" "<<prenume<<" Clasa "<<numar_clasa<<" "<<litera_clasa<<"\n\n";
 }
 
 Elev::Elev(std::string nume, std::string prenume){
-    std::cout<< "Elevul:";
+    std::cout<< "Elevul: ";
     this->nume = nume;
     this->prenume = prenume;
     numar_clasa=-1;
     litera_clasa= "-";
-    std::cout<<this->nume<<" "<<this->prenume<<" Clasa "<<numar_clasa<<litera_clasa;
+    std::cout<<this->nume<<" "<<this->prenume<<" Clasa "<<numar_clasa<<" "<<litera_clasa<<"\n\n";
 }
 
 Elev::Elev(std::string nume, std::string prenume, int numar_clasa){
-    std::cout<< "Elevul:";
+    std::cout<< "Elevul: ";
     this->nume = nume;
     this->prenume = prenume;
     this->numar_clasa = numar_clasa;
     litera_clasa = "-";
-    std::cout<<this->nume<<" "<<this->prenume<<" Clasa "<<this->numar_clasa<<litera_clasa;
+    std::cout<<this->nume<<" "<<this->prenume<<" Clasa "<<this->numar_clasa<<" "<<litera_clasa<<"\n\n";
 }
 
 Elev::Elev(std::string nume, std::string prenume, int numar_clasa, const char* litera_clasa){
-    std::cout<< "Elevul:";
+    std::cout<< "Elevul: ";
     this->nume = nume;
     this->prenume = prenume;
     this->numar_clasa = numar_clasa;
     this->litera_clasa = litera_clasa;
-    std::cout<<this->nume<<" "<<this->prenume<<" Clasa "<<this->numar_clasa<<this->litera_clasa;
+    std::cout<<this->nume<<" "<<this->prenume<<" Clasa "<<this->numar_clasa<<" "<<this->litera_clasa<<"\n\n";
 }
 
 Elev::Elev(Elev &elev){
@@ -71,17 +71,22 @@ Elev::Elev(Elev &elev){
     numar_clasa = elev.numar_clasa;
     litera_clasa = elev.litera_clasa;
 
-    std::cout<<this->nume<<" "<<this->prenume<<" Clasa "<<numar_clasa<<litera_clasa;
+    std::cout<<this->nume<<" "<<this->prenume<<" Clasa "<<this->numar_clasa<<" "<<this->litera_clasa<<"\n\n";
 }
 
 Elev::~Elev(){
-    std::cout<<"\n Sfarsitul programului\n";
+    std::cout<<"Datele elevului au fost afisate."<<"\n\n\n";
 }
 
 
 int main() {
-    Elev e("Andrei","Vasile",12,"B");
 
+    Elev();
+    Elev elev_chelu("Chelu");
+    Elev elev_ion("Ion","Alex");
+    Elev elev_popescu("Popescu","Andreea",10);
+    Elev elev_andrei("Andrei","Vasile",12,"B");
+    Elev elev_copie(elev_andrei);
 
     return 0;
 }
